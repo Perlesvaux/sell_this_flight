06fc6ee105beb8629e713559f53c3623bed41d8b
Perlesvaux <99514791+Perlesvaux@users.noreply.github.com>
2023-01-01 21:14:05 -0600

    first commit :D!!! it works great so far OMG

---
+++ flts.py
@@ -0,0 +1,191 @@
+vuelo_nonstop = """Houston to San Salvador
+10:00am - 1:04pm (3h 4m, nonstop)10:00am through 1:04pm (3h 4m, nonstop)
+
+United • Thu, Jan 26
+Departure10:00am - Houston
+George Bush Intercontinental (IAH)
+
+3h 4m flight
+United 1441
+Boeing 737-900WiFi, entertainment and power on this flight
+Economy/Coach (K)
+Arrival1:04pm - San Salvador
+El Salvador Intl. (SAL)
+
+Hide details"""
+
+
+vuelo_1stop = """
+Houston to San Salvador
+11:01am - 1:10pm (26h 9m, 1 stop)11:01am through 1:10pm (26h 9m, 1 stop)+1Arrives Fri, Jan 13
+
+Aeromexico • Thu, Jan 12
+Departure11:01am - Houston
+George Bush Intercontinental (IAH)
+
+2h 29m flight
+Aeromexico 479
+BOEING 737-800 SCIMITAR WINGLETSEntertainment on this flight
+Economy/Coach (V)
+Arrival1:30pm - Mexico City
+Mexico City Intl. (MEX)
+
+Layover: 21h 15m in Mexico City
+Departure10:45am - Mexico City
+Mexico City Intl. (MEX)
+
+2h 25m flight
+Aeromexico 628 operated by AEROLITORAL DBA AEROMEXICO CONNECT
+Embraer 190Entertainment on this flight
+Economy/Coach (V)
+Arrival1:10pm - San Salvador
+El Salvador Intl. (SAL)
+"""
+
+vuelo_con_escala = """Houston to San Salvador
+7:00am - 1:10pm (6h 10m, 1 stop)7:00am through 1:10pm (6h 10m, 1 stop)
+
+Aeromexico • Thu, Jan 12
+Departure7:00am - Houston
+George Bush Intercontinental (IAH)
+
+2h 30m flight
+Aeromexico 477 operated by AEROLITORAL DBA AEROMEXICO CONNECT
+Embraer 190Entertainment on this flight
+Economy/Coach (V)
+Arrival9:30am - Mexico City
+Mexico City Intl. (MEX)
+
+Layover: 1h 15m in Mexico City
+Departure10:45am - Mexico City
+Mexico City Intl. (MEX)
+
+2h 25m flight
+Aeromexico 628 operated by AEROLITORAL DBA AEROMEXICO CONNECT
+Embraer 190Entertainment on this flight
+Economy/Coach (V)
+Arrival1:10pm - San Salvador
+El Salvador Intl. (SAL)
+
+Hide details"""
+
+
+vuelo_3stop = """
+Multiple airlines • Thu, Jan 12
+Departure7:00am - Houston
+George Bush Intercontinental (IAH)
+
+4h 55m flight
+Alaska Airlines 453
+Power on this flight
+Economy/Coach (Y)
+Arrival9:55am - Seattle
+Seattle - Tacoma Intl. (SEA)
+
+Layover: 8h 10m in Seattle
+Departure6:05pm - Seattle
+Seattle - Tacoma Intl. (SEA)
+
+10h 35m flight
+Condor 2033
+Power on this flight
+Economy/Coach (Y)
+Arrival1:40pm - Frankfurt
+Frankfurt Intl. (FRA)
+
+Arrives Fri, Jan 13
+Layover: 3h 10m in Frankfurt
+Departure4:50pm - Frankfurt
+Frankfurt Intl. (FRA)
+
+1h 20m flight
+Lufthansa 1242
+Power on this flight
+Economy/Coach (Y)
+Arrival6:10pm - Vienna
+Vienna Intl. (VIE)
+
+Hide details"""
+
+"12 12 13 jan"
+
+vuelo_tbs = """Bogotá to Tbilisi
+3:55pm - 5:05pm (40h 10m, 2 stops)3:55pm through 5:05pm (40h 10m, 2 stops)+2Arrives Wed, Mar 8
+
+Turkish Airlines • Mon, Mar 6
+Departure3:55pm - Bogotá
+El Dorado Intl. (BOG)
+
+1h 45m flight
+Turkish Airlines 801
+BOEING 777-300ER
+Economy/Coach (V)
+Arrival5:40pm - Panama City
+Tocumen Intl. (PTY)
+
+Layover: 1h 40m in Panama City
+Departure7:20pm - Panama City
+Tocumen Intl. (PTY)
+
+12h 40m flight
+Turkish Airlines 801
+BOEING 777-300ER
+Economy/Coach (V)
+Arrival4:00pm - Istanbul
+Istanbul Airport (IST)
+
+Arrives Tue, Mar 7
+Layover: 21h 50m in Istanbul
+Departure1:50pm - Istanbul
+Istanbul Airport (IST)
+
+2h 15m flight
+Turkish Airlines 382
+Airbus A321
+Economy/Coach (V)
+Arrival5:05pm - Tbilisi
+Tbilisi Intl. (TBS)
+
+Hide details"""
+
+vuelo_longest = """London to Auckland
+8:00am - 8:45am (60h 45m, 2 stops)8:00am through 8:45am (60h 45m, 2 stops)+3Arrives Sat, Jan 7
+
+Multiple airlines • Wed, Jan 4
+Departure8:00am - London
+Heathrow (LHR)
+
+8h 25m flight
+Austrian Airlines 7791 operated by United
+Boeing 767-300WiFi, entertainment and power on this flight
+Economy/Coach (Y)
+Arrival11:25am - Newark
+Liberty Intl. (EWR)
+
+Layover: 17h 35m in Newark
+Departure5:00am - Newark
+Liberty Intl. (EWR)
+
+4h 11m flight
+American Airlines 536
+Boeing 737-800WiFi, entertainment and power on this flight
+Economy/Coach (Y)
+Arrival8:11am - Dallas
+Dallas-Fort Worth Intl. (DFW)
+
+Layover: 14h 33m in Dallas
+Departure10:44pm - Dallas
+Dallas-Fort Worth Intl. (DFW)
+
+16h 1m flight
+American Airlines 35
+BOEING 787-9WiFi, entertainment and power on this flight
+Premium Economy (W)
+Arrival8:45am - Auckland
+Auckland Intl. (AKL)
+
+Arrives Sat, Jan 7
+Hide details"""
+
+"""['4Jan', '5Jan', '5Jan']"""
+"""['12Jan', '13Jan']"""
